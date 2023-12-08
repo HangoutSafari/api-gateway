dc7ab0df5ae4762461132a7488f722c894e9a189
Simon-Manasse <112821091+Simon-Manasse@users.noreply.github.com>
2023-12-08 15:37:30 +0100

    added proxies

--- code/routes/index.js
+++ code/routes/index.js
@@ -4,11 +4,31 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 const router = express.Router();
 
 // create a proxy for each microservice
-const animalsProxy = createProxyMiddleware({
-  target: 'http://msanimals:3010',
-  changeOrigin: true,
+
+const eventsProxy = createProxyMiddleware({
+    target: 'http://msevents:3011',
+    changeOrigin: true,
 });
 
+const usersProxy = createProxyMiddleware({
+    target: 'http://msusers:3012',
+    changeOrigin: true,
+});
+
+const animalsProxy = createProxyMiddleware({
+    target: 'http://msanimals:3013',
+    changeOrigin: true,
+});
+
+const modelsProxy = createProxyMiddleware({
+    target: 'http://msmodels:3014',
+    changeOrigin: true,
+});
+
+
+router.use('/events', cors(), eventsProxy);
 router.use('/animals', cors(), animalsProxy);
+router.use('/users', cors(), usersProxy);
+router.use('/models', cors(), modelsProxy);
 
 export default router;
--- code/start.js
+++ code/start.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 
-app.set('port', process.env.PORT || 3011);
+app.set('port', process.env.PORT || 3010);
 const server = app.listen(app.get('port'), () => {
   console.log(`🍿 Express running → PORT ${server.address().port}`);
 });
